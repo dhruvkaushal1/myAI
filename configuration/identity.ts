55e5e03903ec876f25cbea2d9bcef58075725ba0
dhruvkaushal1 <89365437+dhruvkaushal1@users.noreply.github.com>
2025-03-17 23:04:06 -0400

    Update identity.ts
    added author info

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Dhruv Kaushal`;
+export const OWNER_DESCRIPTION: string = `Student at UNC Chapel Hill - fellow friend who needed help with interviews`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
