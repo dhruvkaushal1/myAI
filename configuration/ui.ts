3729fc947631d225a8c8fddd5140a914ae1f5ed1
dhruvkaushal1 <89365437+dhruvkaushal1@users.noreply.github.com>
2025-03-18 10:24:55 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `InterviewPro`;
-export const MESSAGE_PLACEHOLDER: string = `Hello! I'm InterviewPro, I'm here to help you prepare for your behavioral interview. I can give you tips on crafting your responses based on experiences, take you through the STAR framework, and interview you with feedback.`;
+export const MESSAGE_PLACEHOLDER: string = `Chat with ${AI_NAME}`;
 export const FOOTER_MESSAGE: string = `Customized AI by Dhruv at UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `InterviewPro's Interview Prep`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
